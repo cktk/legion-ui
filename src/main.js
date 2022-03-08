8a85c37c9703f53957dcc0b65e71ea6082d1d65f
呆猫 <cktk@qq.com>
2022-03-09 00:31:14 +0800

    ⚡️  去掉CDN加速

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ViewUI from 'view-design'
-// import 'view-design/dist/styles/iview.css'
+import 'view-design/dist/styles/iview.css'
 import App from './App'
 import { router } from './router/index'
 import store from './store'
